beb424034c0dc863e4611e5c8546578bb60879a1
Aman Karn <31928020+amankarn26@users.noreply.github.com>
2017-09-30 09:45:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,18 @@ console.log('Loaded!');
 //Changing some data
 var ele = document.getElementById('ele-text');
 ele.innerHTML = "new value";
+
+//move img
+
+var img = document.getElementById('imgDev');
+
+var vari = 0;
+
+function moveDown() {
+    vari = vari + 10;
+    img.style.margintop = vari + 'px';
+}
+
+img.onclick = function() {
+    var interval = setInterval(moveDown, 50);
+};

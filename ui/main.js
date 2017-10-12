5bbe796dd39219d9653638e6c9e567c51b828d73
Aman Karn <31928020+amankarn26@users.noreply.github.com>
2017-10-12 14:25:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,13 +15,13 @@ function moveDown () {
     
     if(inc<=35)
     {   
-        vari = vari + 1;
+        vari = vari + 5;
         inc++;
         img.style.marginTop = vari + 'px';
     }
     if(inc>35&&inc<70)
     {
-        vari = vari - 1;
+        vari = vari - 5;
         inc++;
         img.style.marginTop = vari + 'px';
     }
